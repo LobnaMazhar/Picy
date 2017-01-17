33cb20743fd381bd7068a0f8923666b171c50a83
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-01-18 00:58:40 +0200

    Modified design (splash-screen ,, appIcon ,, colors)

---
+++ Flicker/app/src/main/java/com/example/lobna/flickr/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.lobna.flickr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); //so it won't be stacked behind main
+            }
+        }, 3000);
+    }
+}
